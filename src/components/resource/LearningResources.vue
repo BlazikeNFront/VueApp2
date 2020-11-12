ddbe06a4670f04231b43bf71382562f78827d265
Damian <67591714+BlazikeNFront@users.noreply.github.com>
2020-11-12 20:27:44 +0100

    Add files via upload

---
+++ src/components/resource/LearningResources.vue
@@ -0,0 +1,24 @@
+<template>
+        <single-resource v-for='res in resources' :key='res.id' :desc='res.desc' :title='res.title' :link="res.link"></single-resource>
+</template>
+
+
+<script>
+import SingleResource from './SingleResource.vue';
+
+
+
+export default {
+    components:{
+        SingleResource,
+      
+    },
+  inject:{
+      resources:['resources']
+  }
+    
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/resource/TheResources.vue
@@ -0,0 +1,62 @@
+<template>
+ <base-card>
+    <base-button @click="setSelectedTab('learning-resources')" :mode='storedResButtonMode'>Learning resources</base-button>
+
+    <base-button @click='setSelectedTab("add-resources")' :mode='addResButtonMode'> Add resources</base-button>
+    <component :is='selectedTab'></component>
+</base-card>
+</template>
+
+<script>
+import LearningResources from './LearningResources.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+    components:{
+         LearningResources,
+         AddResources,
+    },
+    computed:{
+        storedResButtonMode(){
+           return  this.selectedTab === 'learning-resources'? null :"flat"
+        },
+         addResButtonMode(){
+           return  this.selectedTab === 'add-resources'? null :"flat"
+        },
+    },
+    data(){
+     return {
+        selectedTab:'learning-resources',
+        storedResource:[
+                {
+                    id:'official-guide',
+                    title:'Official VUE guide',
+                    desc:'Official Vue framework documentation',
+                    link:'https://vuejs.org/',
+                },
+                {
+                    id:'google',
+                    title:'Google',
+                    desc:'Learn to google...',
+                    link:'https://www.google.com/',
+                },
+                ],
+     
+    }
+    },
+    provide(){
+       return { resources:this.storedResource,}
+    },
+
+    methods:{
+        setSelectedTab(tab){
+            this.selectedTab = tab;
+        }
+    },
+    
+}
+</script>
+
+<style  scoped>
+
+</style>
